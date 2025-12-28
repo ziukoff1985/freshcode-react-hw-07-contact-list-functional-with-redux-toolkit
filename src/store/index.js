e7c75b8b31792d623f4510effe8a6f663ef43d68
Borys Ziukov <ziukoff1985@gmail.com>
2025-12-28 10:01:45 +0200

    Refactore store/index.js with configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,21 @@
-import { createStore } from 'redux';
-import { applyMiddleware } from 'redux'; // метод для додавання middleware
+// *** OLD IMPORTS WITHOUT REDUX TOOLKIT ***
+// import { createStore } from 'redux';
+// import { applyMiddleware } from 'redux'; // метод для додавання middleware
+// import { composeWithDevTools } from '@redux-devtools/extension'; // для роботи Redux DevTools
+// import logger from 'redux-logger'; // для middleware - logger
+// import contactsReducer from './reducers/contactsReducer';
+// *** =================================== ***
+import { configureStore } from '@reduxjs/toolkit';
 
-import { composeWithDevTools } from '@redux-devtools/extension'; // для роботи Redux DevTools
-import logger from 'redux-logger'; // для middleware - logger
+import contactsReducer from './slices/contactsSlice';
 
-import contactsReducer from './reducers/contactsReducer';
+export default configureStore({
+    reducer: {
+        contactsList: contactsReducer,
+    },
+});
 
-const middleware = applyMiddleware(logger); // додавання middleware logger
+// *** OLD CODE WITHOUT REDUX TOOLKIT ***
+// const middleware = applyMiddleware(logger); // додавання middleware logger
 
-export default createStore(contactsReducer, composeWithDevTools(middleware));
+// export default createStore(contactsReducer, composeWithDevTools(middleware));
