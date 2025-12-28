fd35b7133208a8c71ebfc9207fc0af1bce0a4172
Borys Ziukov <ziukoff1985@gmail.com>
2025-12-28 10:53:54 +0200

    Add middleware 'logger' in store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,19 @@
 // import { createStore } from 'redux';
 // import { applyMiddleware } from 'redux'; // метод для додавання middleware
 // import { composeWithDevTools } from '@redux-devtools/extension'; // для роботи Redux DevTools
-// import logger from 'redux-logger'; // для middleware - logger
 // import contactsReducer from './reducers/contactsReducer';
 // *** =================================== ***
 import { configureStore } from '@reduxjs/toolkit';
 
 import contactsReducer from './slices/contactsSlice';
 
+import logger from 'redux-logger'; // для middleware - logger
+
 export default configureStore({
     reducer: {
         contactsList: contactsReducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 // *** OLD CODE WITHOUT REDUX TOOLKIT ***
